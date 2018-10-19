d9faaa973fd84283780ec94825e1d34d044adeba
Machoman <rbunnicelli2010@yahoo.com>
2018-10-18 22:10:58 -0400

    controller and models done

--- controller/api.js
+++ controller/api.js
@@ -13,7 +13,166 @@ app.get("/scrape", function(req, res) {
 
         //grab articles within page with h2 element
         $("article").each(function(i, element) {
+            //empty array for results
             var result = {};
+                //title
+                result.title = $(element)
+                    .children('story-heading')
+                    .children('a')
+                    .text().trim();
+                //link
+                result.link = $(element)
+                    .children('.story-heading')
+                    .children('a')
+                    .attr("href");
+                //summary
+                    result.summary = $(element)
+                    .children('.summary')
+                    .text().trim()
+                    || $(element)
+                        .children('ul')
+                        .text().trim();
+
+    // Create a new Article using the `result` object built from scraping
+    db.Article.create(result)
+    .then(function(dbArticle) {
+        // View the added result in the console
+        console.log(dbArticle);
         })
-    })
-})
+        .catch(function(err) {
+        // If an error occurred, send it to the client
+        return res.json(err);
+            });
+        });
+        // If we were able to successfully scrape and save an Article, send a message to the client
+        res.send("Scrape Complete");
+    });
+});
+
+//Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+      .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+//Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+      // ..and populate all of the notes associated with it
+      .populate("note")
+      .then(function(dbArticle) {
+        // If we were able to successfully find an Article with the given id, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+//Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        // If we were able to successfully update an Article, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+//delete an article
+app.post("/articles/delete/:id", function(req, res) {
+    // Use the article id to find and update its saved boolean
+    Article.findOneAndUpdate({ "_id": req.params.id }, {"saved": false, "notes": []})
+    // Execute the above query
+    .exec(function(err, doc) {
+      // Log any errors
+      if (err) {
+        console.log(err);
+      }
+      else {
+        // Or send the document to the browser
+        res.send(doc);
+      }
+    });
+});
+
+//create a new note
+app.post("/notes/save/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    var newNote = new Note({
+      body: req.body.text,
+      article: req.params.id
+    });
+    console.log(req.body)
+    // And save the new note the db
+    newNote.save(function(error, note) {
+      // Log any errors
+      if (error) {
+        console.log(error);
+      }
+      // Otherwise
+      else {
+        // Use the article id to find and update it's notes
+        Article.findOneAndUpdate({ "_id": req.params.id }, {$push: { "notes": note } })
+        // Execute the above query
+        .exec(function(err) {
+          // Log any errors
+          if (err) {
+            console.log(err);
+            res.send(err);
+          }
+          else {
+            // Or send the note to the browser
+            res.send(note);
+          }
+        });
+      }
+    });
+  });
+  
+//delete a note
+app.delete("/notes/delete/:note_id/:article_id", function(req, res) {
+    // Use the note id to find and delete it
+    Note.findOneAndRemove({ "_id": req.params.note_id }, function(err) {
+      // Log any errors
+      if (err) {
+        console.log(err);
+        res.send(err);
+      }
+      else {
+        Article.findOneAndUpdate({ "_id": req.params.article_id }, {$pull: {"notes": req.params.note_id}})
+         // Execute the above query
+          .exec(function(err) {
+            // Log any errors
+            if (err) {
+              console.log(err);
+              res.send(err);
+            }
+            else {
+              // Or send the note to the browser
+              res.send("Note Deleted");
+            }
+          });
+      }
+    });
+});
---
+++ models/article.js
@@ -0,0 +1,43 @@
+//mongoose
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    link: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    
+    summary: {
+        type: String,
+        required: false,
+        unique: false
+    },
+
+    isSaved: {
+        type: Boolean,
+        default: false,
+        required: false,
+        unique: false
+    },
+
+    note: {
+        type: [{ type: Schema.Types.ObjectId, ref: 'Note'}],
+    }    
+});
+
+//article model with Article Schema
+var Article = mongoose.model("Article", ArticleSchema);
+
+//export
+module.exports = Article;
+
